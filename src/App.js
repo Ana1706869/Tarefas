691be6251b448980d83e641ae644ff077d3bc874
Ana Silva <anasilva_pinhel@hotmail.com>
2025-10-04 16:07:43 +0100

    Realização do frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  const [tarefas,setTarefas]=useState([])
+  const [titulo,setTitulo]=useState("")
 
-export default App;
+  const carregarTarefas=()=>{
+    fetch("http://127.0.0.1:8000/tarefas").then(res=>res.json())
+    .then(data=>setTarefas(data))
+  }
+  useEffect(()=>{
+    carregarTarefas()
+  },[])
+
+  const adicionarTarefa=()=>{
+    fetch("http://127.0.0.1:8000/tarefas",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body: JSON.stringify({titulo})
+    }).then(()=>{
+      setTitulo("")
+      carregarTarefas()
+    })
+
+  }
+  const removerTarefa=(id)=>{
+    fetch(`http://127.0.0.1:8000/tarefas/${id}`,{
+      method: "DELETE"}).then(()=>carregarTarefas())
+    }
+    return(
+      <div style={{padding:20}}>
+        <h1>Lista de Tarefas</h1>
+        <input value={titulo} onChange={(e)=>setTitulo(e.target.value)} placeholder="Nova tarefa"/>
+        <button onClick={adicionarTarefa}>Adicionar</button>
+        <ul>
+          {tarefas.map((tarefa)=>(
+            <li key={tarefa.id}>{tarefa.titulo}
+            <button onClick={()=>removerTarefa(tarefa.id)}>Remover</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+  export default App
+
+  
+
